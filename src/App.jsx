723e7a58f03feacf2d27723a68627faca47ed1d7
Josh Wang <Joshw9012@gmail.com>
2023-12-04 05:35:48 +1100

    325 - updating Data without Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CreateOrder, {
   action as createOrderAction,
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
 import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
@@ -37,6 +38,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction, //on the child component of the main page - OrderItem is a child component of <Order />
       },
     ],
   },
